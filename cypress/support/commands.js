813d5d3fd27c57fe9ff13c81304f07aacd5b529e
elenavoitova <elena.voitova.lb@gmail.com>
2018-12-21 12:36:28 +0200

    Update commands.js

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -15,17 +15,3 @@ Cypress.Commands.add('addFilter', (name, city) => {
     cy.get(filter.cityInput).type(city)
     cy.get(filter.submitButton).click()
 });
- //Cypress.$(filter.submitButton).click()
-//  cy.get($searchCity).type(city)
-//  cy.get($submit).click()
-//  return cy.get($person).its('length')
-  // let items = cy.get($person).its('length')
-  // if (items.length == 0){
-  //     return items.length;
-  // } else {
-  //     let person = cy.get($name)
-  //     return result = {
-  //               count: items.length,
-  //               person: person
-  //   }
-  // }
