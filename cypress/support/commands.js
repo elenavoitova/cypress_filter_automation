e30ddf438df40fdc7dc596d7886e4d894b3f3485
elenavoitova <elena.voitova.lb@gmail.com>
2019-01-02 15:06:15 +0200

    Update commands.js

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,17 +1,12 @@
 import { candidate, filter, hiringSection } from '../fixtures/locators';
 
-/*
-* Selectors
-* filter.clearButton
-* filter.submitButton
-* filter.nameInput
-* filter.cityInput
-* candidate.profileName
-* candidate.profileImage
-* candidate.profileContainer
-*/
 Cypress.Commands.add('addFilter', (name, city) => {
-    cy.get(filter.nameInput).type(name)
-    cy.get(filter.cityInput).type(city)
-    cy.get(filter.submitButton).click()
+  cy.get(filter.name).type(name)
+  cy.get(filter.city).type(city)
+  cy.get(filter.submit).click()
+});
+
+Cypress.Commands.add('checkProfile', (name, city) => {
+  if (name !=='{enter}') cy.get(candidate.fullName).contains(name)
+  if (city !=='{enter}') cy.get(candidate.city).contains(city)
 });
